6f509ce6e3141a804a2b23ea98914d6617a1a25c
大嶋由真 <>
2022-06-15 00:16:15 +0900

    課題修正-2

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-const charts =['====================', '現在持っているのタスク一覧', '===================='];
+const charts = [
+  '====================',
+  '現在持っているのタスク一覧',
+  '====================',
+];
 const todos = ['掃除', '買い物', '散歩'];
 
-charts.forEach ((chart)=> console.log (chart));
-todos.forEach ((value, index) => console.log (`${index}: ${value}`));
+charts.forEach((chart) => console.log(chart));
+todos.forEach((value, index) => console.log(`${index}: ${value}`));
 
-const put = prompt ('「確認、追加、削除、終了」の４つのいずれかを入力してください');
+const put = prompt(
+  '「確認、追加、削除、終了」の４つのいずれかを入力してください'
+);
 
 if (put === '追加') {
-  document.write();
-  task = prompt ('タスクを入力してください'); 
-  todos.push(task)
-  cmp = alert ('新しいタスクを追加しました');
+  const task = prompt('タスクを入力してください');
+  todos.push(task);
+  const cmp = alert('新しいタスクを追加しました');
 
-  charts.forEach ((chart)=> console.log(chart));
-  todos.forEach ((value, index) => console.log(`${index}: ${value}`));
-  while (true){
-    const check = confirm ('さらに追加しますか？');
+  charts.forEach((chart) => console.log(chart));
+  todos.forEach((value, index) => console.log(`${index}: ${value}`));
+  while (true) {
+    const check = confirm('さらに追加しますか？');
     if (check === true) {
-      document.write();
-      retask = prompt ('タスクを入力してください'); 
-      todos.push(retask)
-      cmp = alert ('新しいタスクを追加しました');
-  
-      charts.forEach ((chart)=> console.log(chart));
-      todos.forEach ((value, index) => console.log(`${index}: ${value}`));
-    }else {
+      retask = prompt('タスクを入力してください');
+      todos.push(retask);
+      const cmpp = alert('新しいタスクを追加しました');
+
+      charts.forEach((chart) => console.log(chart));
+      todos.forEach((value, index) => console.log(`${index}: ${value}`));
+    } else {
       break;
     }
   }
 }
-
